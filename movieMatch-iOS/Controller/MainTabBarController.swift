8d1393d6f6faef485c46f76a27a336c0c901bd39
alex99091 <bkk99091@gmail.com>
2023-03-02 00:34:42 +0900

    basic project environment Set up

---
+++ movieMatch-iOS/Controller/MainTabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabBarController.swift
+//  movieMatch-iOS
+//
+//  Created by BOONGKI KWAK on 2023/03/02.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
